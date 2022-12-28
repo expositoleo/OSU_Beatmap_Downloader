66fa78e6a193fe33eabe9c99ff6f431d3016d412
Leonardo Expósito <89589981+expositoleo@users.noreply.github.com>
2022-12-28 13:43:19 -0300

    Add persistence.py

---
+++ persistence.py
@@ -0,0 +1,7 @@
+class Persistence:
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def update(self):
+        self.localdata = "update local data logic here"
+        return self.localdata
