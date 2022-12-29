c481c757093c2f1165715333940f372021613d14
Leonardo Expósito <89589981+expositoleo@users.noreply.github.com>
2022-12-29 13:36:18 -0300

    Add compare_beatmap method

--- expert.py
+++ expert.py
@@ -1,11 +1,24 @@
 class Expert:
     def __init__(self, screen, persistence):
+        # Associations
         self.screen = screen
         self.persistence = persistence
-        self.beatmaps_finder_factory = BeatmapsFinderFactory()
+        self.beatmap_finder_factory = BeatmapFinderFactory()
 
-    def find_beatmaps(self, inputs):
-        beatmap_finder = self.beatmaps_finder_factory.create_beatmap_finder(inputs)
-        beatmaps_list = beatmap_finder.find_beatmaps()
-        self.beatmaps_list = beatmaps_list
-        # downloader = Downloader(bm_list)
+    def find_beatmap(self, inputs):
+        # Instance strategy factory, get the beatmap_list to download and call comparation method
+        beatmap_finder = self.beatmap_finder_factory.create_beatmap_finder(inputs)
+        beatmap_list = beatmap_finder.find_beatmap()
+        self.beatmap_list = beatmap_list
+        self.compare_beatmap(beatmap_list)
+
+    def compare_beatmap(self, beatmap_list: list[str]):
+        # Get local beatmaps and compare with the given list
+        self.beatmap_local = self.persistence.beatmap_local
+        for i in beatmap_list:
+            while i in beatmap_local:
+                beatmap_list = beatmap_list.remove(i)
+
+        # Instance and call downloader
+        self.downloader = Downloader(self)
+        self.downloader.download(beatmap_list)
