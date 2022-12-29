b3c76caaec72f0681b0ff39c914bde488b3837bc
Leonardo Expósito <89589981+expositoleo@users.noreply.github.com>
2022-12-29 13:35:31 -0300

    Add downloader.py

---
+++ downloader.py
@@ -0,0 +1,11 @@
+class Downloader:
+    def __init__(self, expert):
+        self.expert = expert
+
+    def download(self, beatmap_list):
+        "Download process goes here"
+        pass
+
+    def download_status(self, beatmap_list):
+        "Show all successful downloads"
+        pass
